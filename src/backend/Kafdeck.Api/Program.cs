b215b2ec6c1ffca35862311849a5d5b76cd19740
Ammar Heidari <ammar@arad-itc.org>
2026-09-17 23:19:31 +0330

    feat(api): wire W08 HTTP and observability runtime
    Signed-off-by: Ammar Heidari <ammar@arad-itc.org>

--- src/backend/Kafdeck.Api/Program.cs
+++ src/backend/Kafdeck.Api/Program.cs
@@ -1,6 +1,17 @@
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Kafdeck.Api;
 using Kafdeck.Core;
+using Kafdeck.Core.Kafka;
 using Kafdeck.Infrastructure.Configuration;
+using Kafdeck.Infrastructure.Kafka;
 using Kafdeck.Infrastructure.Security;
+using Kafdeck.Modules.Clusters;
+using Kafdeck.Modules.Topics;
+
+Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+Activity.ForceDefaultIdFormat = true;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +24,24 @@ var secretResolver = new SecretResolver();
 var deploymentAccessToken = kafdeckOptions.Deployment.AccessToken is null
     ? null
     : secretResolver.Resolve(kafdeckOptions.Deployment.AccessToken).Reveal();
+
+builder.Services.AddProblemDetails();
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+});
+
+builder.Services.AddSingleton(kafdeckOptions);
+builder.Services.AddSingleton(secretResolver);
+builder.Services.AddSingleton<KafkaSnapshotPolicy>();
+builder.Services.AddSingleton<KafkaSnapshotCoordinator>(services =>
+    new KafkaSnapshotCoordinator(services.GetRequiredService<KafkaSnapshotPolicy>()));
+builder.Services.AddSingleton<IKafkaAdministrationPort>(_ =>
+    new ConfluentKafkaAdministrationAdapter(kafdeckOptions.Clusters, secretResolver));
+builder.Services.AddSingleton<ClusterExplorerService>();
+builder.Services.AddSingleton<TopicExplorerService>();
+builder.Services.AddSingleton<ApiTelemetry>();
 
 var app = builder.Build();
 
@@ -29,16 +58,22 @@ foreach (var cluster in kafdeckOptions.Clusters.Where(cluster =>
         cluster.SecurityProtocol);
 }
 
+app.UseExceptionHandler();
+app.UseMiddleware<ApiTelemetryMiddleware>();
+
 if (deploymentAccessToken is not null)
 {
     app.UseMiddleware<DeploymentAccessTokenMiddleware>(deploymentAccessToken);
 }
 
 app.MapGet("/healthz", () => Results.Ok(new
-{
-    status = "ok",
-    product = ProductIdentity.Name,
-}));
+    {
+        status = "ok",
+        product = ProductIdentity.Name,
+    }))
+    .WithName("healthz");
+
+app.MapKafdeckV01(kafdeckOptions);
 
 app.Run();
 
